b7863d416ebb0d253690ed0aea1d01d6eb638212
michaelecolley <michael.e.colley@googlemail.com>
2021-09-13 21:28:19 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const app = express();
 
 //middleware
 /* app.use(cors());
- */app.use(express.json());
+ */ app.use(express.json());
 /* app.use(cookieParser());
- */app.use(express.urlencoded({ extended: true }));
+ */ app.use(express.urlencoded({ extended: true }));
 
 //serve css files
 app.get('/css/style.css', (_req, res) => {
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 
 //message router
 /* app.use('/messages', messageRouter);
- *///auth router
+ */ //auth router
 //app.use('/authenticate', authRouter);
 
 //global error handler
@@ -41,7 +41,6 @@ app.use((err, req, res, next) => {
   const errorObj = Object.assign({}, defaultErr, err);
   return res.status(errorObj.status).json(errorObj.message);
 });
-
 
 //initialize server
 const PORT = 4000;
