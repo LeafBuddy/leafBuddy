a32644e7a2fa8baa80c0afaa49847870a151e0a2
michaelecolley <michael.e.colley@googlemail.com>
2021-09-13 21:27:58 +0100

    Create Transaction.jsx

---
+++ client/Components/Transaction.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Transaction = (props) => {
+  return (
+    <div style={{ backgroundColor: 'purple', color: 'white' }}>
+      <h2>Transaction</h2>
+      <h3>Icon</h3>
+      <h3>Merchant</h3>
+      <h3>Currency</h3>
+      <h3>Amount</h3>
+      <h3>Carbon Amount</h3>
+    </div>
+  );
+};
+export default Transaction;
