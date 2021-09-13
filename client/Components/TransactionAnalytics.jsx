7f9da13805cd380256df0cd85fb755fefca17c10
michaelecolley <michael.e.colley@googlemail.com>
2021-09-13 21:28:01 +0100

    Create TransactionAnalytics.jsx

---
+++ client/Components/TransactionAnalytics.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TransactionAnalytics = (props) => {
+  return (
+    <div>
+      <h2>TransactionAnalytics</h2>
+      <h1>Total Carbon footprint last month</h1>
+      <h1>7d moving average of carbon footprint</h1>
+      <h1>Timeseries of carbon footprint purchases</h1>
+    </div>
+  );
+};
+export default TransactionAnalytics;
