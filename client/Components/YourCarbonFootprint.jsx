28bf7e795d2317c5495edccab4d5ef46b7f38a28
michaelecolley <michael.e.colley@googlemail.com>
2021-09-14 21:21:56 +0100

    Update login style

---
+++ client/Components/YourCarbonFootprint.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const YourCarbonFootprint = () => {
+  return (
+    <div className='rightChart'>
+      <div className='footprintNumber'>{529}</div>
+      <div className='footprintMeasure'>kg</div>
+      <div className='footprintLabel'>Your Carbon Footprint</div>
+    </div>
+  );
+};
+export default YourCarbonFootprint;
