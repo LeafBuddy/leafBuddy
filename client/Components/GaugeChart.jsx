f86fed3463952160d9c59ad64fc397e97cd2e496
michaelecolley <michael.e.colley@googlemail.com>
2021-09-14 21:20:22 +0100

    remove redundant charts

--- client/Components/GaugeChart.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const data = [{ value: 527.65 }, { value: 602.35 }];
-
-const GaugeChart = () => {
-  return <div>Chart 1</div>;
-};
-export default GaugeChart;
--- client/Components/LineChart.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const data = [{ value: 527.65 }, { value: 602.35 }];
-
-const LineChart = () => {
-  return <div>Chart 3</div>;
-};
-export default LineChart;
