1ef78dd69502e63ef67b26b61b76b53b4c10f8d9
ronke11 <rsoyekunle@gmail.com>
2021-09-15 12:28:49 -0400

    Moves controllers

--- server/routes/plaidRouter.js
+++ server/routes/plaidRouter.js
@@ -3,6 +3,7 @@ const plaidRouter = express.Router();
 const plaidControllers = require('../controllers/plaidControllers.js');
 const carbonControllers = require('../controllers/carbonControllers.js');
 const mccControllers = require('../controllers/mastercardCodes.js');
+const carbonScore = require('../controllers/carbonScore.js');
 
 
 plaidRouter.get('/linkToken', plaidControllers.createLinkToken, (req, res) => {
@@ -15,19 +16,25 @@ plaidRouter.post('/publicToken', plaidControllers.publicToken, (req, res) => {
   res.status(200).json(res.locals.);
 });
 
-plaidRouter.get('/transactions', plaidControllers.getTransactions, (req, res) => {
-  console.log('hitting transactions router');
-  res.status(200).json(res.locals.transactions);
+plaidRouter.get('/transactions', plaidControllers.getTransactions, carbonScore.categories, carbonScore.score, (req, res) => {
+  res.status(200).json(res.locals.completelist);
 });
 
-plaidRouter.get('/carbon', carbonControllers.carbon, (req, res) => {
-  console.log('hitting carbon router');
-  res.status(200).json('finished carbon router');
-});
+// plaidRouter.get('/carbon', carbonControllers.carbon, (req, res) => {
+//   console.log('hitting carbon router');
+//   res.status(200).json('finished carbon router');
+// });
 
 plaidRouter.get('/codes', mccControllers.getCode, (req, res) => {
   console.log('hitting mcc router');
   res.status(200).json('finished mcc router');
 });
+
+plaidRouter.get('/score', carbonScore.score, (req, res) => {
+  console.log('hitting score router');
+  res.status(200).json('hit categories');
+});
+
+
 
 module.exports = plaidRouter;
