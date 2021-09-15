e4c95ed55deca98e267ed0d470162666ca0bf95b
michaelecolley <michael.e.colley@googlemail.com>
2021-09-15 20:35:18 +0100

    Update carbonfootprint component

--- client/Components/YourCarbonFootprint.jsx
+++ client/Components/YourCarbonFootprint.jsx
@@ -1,13 +1,17 @@
 const React = require('react');
+const { useEffect } = require('react');
 
-const YourCarbonFootprint = () => {
+const YourCarbonFootprint = (props) => {
+  useEffect(() => {
+    console.log('Your carbonFootprint:', props.props.carbonFootPrint7d);
+  });
   return (
     <div className='rightChart'>
-      <div className='footprintNumber'>{529}</div>
+      <div className='footprintNumber'>{props.props.carbonFootPrint7d}</div>
       <div className='footprintMeasure'>kg</div>
       <div className='footprintLabel'>Your Carbon Footprint</div>
     </div>
   );
-}
+};
 
 module.exports = YourCarbonFootprint;
