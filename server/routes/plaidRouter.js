841d38c3d7e5b3cb9df8d00b89d750c9992c343a
ronke11 <rsoyekunle@gmail.com>
2021-09-15 14:42:08 -0400

    Completes logic for carbon calculation

--- server/routes/plaidRouter.js
+++ server/routes/plaidRouter.js
@@ -1,8 +1,8 @@
 const express = require('express'); 
 const plaidRouter = express.Router(); 
 const plaidControllers = require('../controllers/plaidControllers.js');
-const carbonControllers = require('../controllers/carbonControllers.js');
-const mccControllers = require('../controllers/mastercardCodes.js');
+// const carbonControllers = require('../controllers/carbonControllers.js');
+// const mccControllers = require('../controllers/mastercardCodes.js');
 const carbonScore = require('../controllers/carbonScore.js');
 
 
@@ -16,8 +16,8 @@ plaidRouter.post('/publicToken', plaidControllers.publicToken, (req, res) => {
   res.status(200).json(res.locals.access_token);
 });
 
-plaidRouter.get('/transactions', plaidControllers.getTransactions, carbonScore.categories, carbonScore.score, (req, res) => {
-  res.status(200).json(res.locals.completelist);
+plaidRouter.get('/transactions', plaidControllers.getTransactions, carbonScore.score, (req, res) => {
+  res.status(200).json(res.locals.carbonlist);
 });
 
 // plaidRouter.get('/carbon', carbonControllers.carbon, (req, res) => {
@@ -25,15 +25,15 @@ plaidRouter.get('/transactions', plaidControllers.getTransactions, carbonScore.c
 //   res.status(200).json('finished carbon router');
 // });
 
-plaidRouter.get('/codes', mccControllers.getCode, (req, res) => {
-  console.log('hitting mcc router');
-  res.status(200).json('finished mcc router');
-});
+// plaidRouter.get('/codes', mccControllers.getCode, (req, res) => {
+//   console.log('hitting mcc router');
+//   res.status(200).json('finished mcc router');
+// });
 
-plaidRouter.get('/score', carbonScore.score, (req, res) => {
-  console.log('hitting score router');
-  res.status(200).json('hit categories');
-});
+// plaidRouter.get('/score', carbonScore.score, (req, res) => {
+//   console.log('hitting score router');
+//   res.status(200).json('hit categories');
+// });
 
 
 
