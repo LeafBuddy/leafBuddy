395bf4fb34e6d2090cdade55fb1fd8ca82da9f8f
michaelecolley <michael.e.colley@googlemail.com>
2021-09-15 20:36:02 +0100

    Create logic for data container

--- client/Container/DataContainer.jsx
+++ client/Container/DataContainer.jsx
@@ -3,45 +3,53 @@ const regeneratorRuntime = require('regenerator-runtime');
 const { useState, useEffect } = require('react');
 const TransactionAnalytics = require('../Container/TransactionAnalytics');
 const TransactionContainer = require('../Container/TransactionContainer');
+const axios = require('axios');
 
 import { WindMillLoading } from 'react-loadingg';
 export default function DataContainer() {
   const [transactions, setTransactions] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [connectedToPlaid, setPlaidConnection] = useState(null);
+
   useEffect(() => {
-    getTransactionData();
+    if (!transactions) {
+      getTransactionData();
+    }
   }, [transactions]);
 
   async function getTransactionData() {
-    try {
-      const data = await axios.get('/plaid/transactions').then((res) => {
-        return [res.data];
-      });
-      setTransactions(data);
-      while (transactions !== null && transactions.length !== 0) {
-        setLoading(true);
-        return;
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    setPlaidConnection('loading');
+    const data = await axios.get('/plaid/transactions').then((res) => {
+      return [res.data];
+    });
+    await setTransactions(data);
+    await setPlaidConnection('loaded');
+    return;
   }
 
   return (
-    <div className='dataContainer'>
-      {loading ? (
-        <div>
-          <TransactionAnalytics props={transactions} />
-          <TransactionContainer props={transactions} />
-        </div>
-      ) : (
-        <div>
-          <WindMillLoading color='white' speed='0.5' />
-          <div>
-            <h1 className='loadingText'>Loading....</h1>
-          </div>
-        </div>
-      )}
+    <div className='mainContainer'>
+      {(() => {
+        switch (connectedToPlaid) {
+          case 'loaded':
+            return (
+              <div className='mainContent'>
+                <TransactionAnalytics props={transactions} />
+                <TransactionContainer props={transactions} />
+                );
+              </div>
+            );
+          case 'loading':
+            return <WindMillLoading color='white' speed='0.5' />;
+          default:
+            return (
+              <div className='DataConnectionModule'>
+                <b>❗ Please connect to your bank</b>
+                <br></br>
+                <h1 style={{ fontSize: '10rem' }}>🍃</h1>
+              </div>
+            );
+        }
+      })()}
     </div>
   );
 }
