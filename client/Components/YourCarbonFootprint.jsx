b75c4b420cb82a89c37deb93b4381c7fe34798b7
michaelecolley <michael.e.colley@googlemail.com>
2021-09-15 22:06:28 +0100

    Implement state mgmt and calculations

--- client/Components/YourCarbonFootprint.jsx
+++ client/Components/YourCarbonFootprint.jsx
@@ -1,15 +1,20 @@
 const React = require('react');
-const { useEffect } = require('react');
+const { useEffect, useState } = require('react');
 
 const YourCarbonFootprint = (props) => {
+  const [transactions] = useState(props.props);
+  const [carbonFootPrint7d, setFootprint] = useState(0);
   useEffect(() => {
-    console.log('Your carbonFootprint:', props.props.carbonFootPrint7d);
+    const result = transactions.reduce((acc, curr) => {
+      return (acc += curr.carbonAmount);
+    }, 0);
+    setFootprint(result);
   });
   return (
     <div className='rightChart'>
-      <div className='footprintNumber'>{props.props.carbonFootPrint7d}</div>
+      <div className='footprintNumber'>{carbonFootPrint7d.toFixed(1)}</div>
       <div className='footprintMeasure'>kg</div>
-      <div className='footprintLabel'>Your Carbon Footprint</div>
+      <div className='footprintLabel'>Your 2 week Carbon Footprint</div>
     </div>
   );
 };
--- client/Container/TransactionAnalytics.jsx
+++ client/Container/TransactionAnalytics.jsx
@@ -2,29 +2,27 @@ const React = require('react');
 const { useEffect, useState } = require('react');
 const YourCarbonFootprint = require('../Components/YourCarbonFootprint');
 const BarChart = require('../Components/BarChart');
+import { WaveLoading } from 'react-loadingg';
 
 const TransactionAnalytics = (props) => {
   const [transactions] = useState(props.props[0]);
-  const weekAgo = Date.now() - 7 * 24 * 60 * 60 * 1000;
   let carbonFootPrint7d = 0;
   useEffect(() => {
-    carbonFootPrint7d = transactions.reduce((acc, curr) => {
-      return Date.parse(curr.transactionDate) > weekAgo
-        ? (acc += curr.carbonAmount)
-        : (acc += 0);
-    }, carbonFootPrint7d);
+    return (carbonFootPrint7d = transactions.reduce((acc, curr) => {
+      return (acc += curr.carbonAmount);
+    }, 0));
   });
 
   return (
     <div className='transactionAnalytics'>
       <div className='transactionAnalyticsHeader'>
-        <h3>Hey Konstantin 👋</h3>
+        <h3>Hey Konstantin 👋, go save a seal</h3>
       </div>
       <div className='charts'>
         <div className='leftChart'>
-          <BarChart />
+          <BarChart props={transactions} />
         </div>
-        <YourCarbonFootprint props={{ carbonFootPrint7d }} />
+        <YourCarbonFootprint props={transactions} />
       </div>
     </div>
   );
